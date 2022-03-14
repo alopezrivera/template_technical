499342f91e80bd8548e6ffa3f8cbb01e8233b433
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2022-03-14 19:57:57 +0100

    Cleanup

--- project/subpackage/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: © 2021 Author <email>
-# SPDX-License-Identifier: GPL-3.0-only
-
-"""
-A Subpackage
-============
-
-As before, this package's ``__init__.py`` is the home of this introduction.
-"""

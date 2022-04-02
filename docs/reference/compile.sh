dbc95b204d569d03cfe42f4442e36fcb001aa0f3
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2022-04-02 18:43:33 +0200

    Test CI

--- docs/reference/compile.sh
+++ docs/reference/compile.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 # Remove any generated documentation
-find . -name 'simple_pendulum*rst'
-find . -name 'simple_pendulum*rst' -delete
-# Clean
+find . -name 'project*rst'
+find . -name 'project*rst' -delete
+# Remove LaTeX artifacts
 make clean
-
-# Generate site
-make html
-find . -name 'simple_pendulum*rst'
-find . -name 'simple_pendulum*rst' -delete
-# Generate LaTeX code
 make latex
-find . -name 'simple_pendulum*rst'
-find . -name 'simple_pendulum*rst' -delete
 
 # Build
 cd build
